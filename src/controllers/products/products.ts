fcf709d59e2989417ec990b1c3c9c1576638a096
unknown <nahuecroc@gmail.com>
2021-06-08 20:14:30 -0300

    Agrego carpeta Data con productos

--- src/controllers/products/products.ts
+++ src/controllers/products/products.ts
@@ -1,32 +1,5 @@
 import express from 'express'
-
-const products = [
-    {
-        name: 'producto1',
-        image: 'imageProducto1',
-        description: 'descripcionProducto1'
-    },
-    {
-        name: 'producto2',
-        image: 'imageProducto2',
-        description: 'descripcionProducto2'
-    },
-    {
-        name: 'producto3',
-        image: 'imageProducto3',
-        description: 'descripcionProducto3'
-    },
-    {
-        name: 'producto4',
-        image: 'imageProducto4',
-        description: 'descripcionProducto4'
-    },
-    {
-        name: 'producto5',
-        image: 'imageProducto5',
-        description: 'descripcionProducto5'
-    },
-]
+const products = require('../../data/products');
 
 function productsController (req:express.Request, res:express.Response){
     //productos de prueba 
@@ -34,4 +7,4 @@ function productsController (req:express.Request, res:express.Response){
     res.send(products)
 }
 
-export default productsController
+export default productsController;
