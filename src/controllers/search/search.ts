36a60dc16f1cbb55fd6f624c6aa1d6fa70f58064
leandrojg <79561670+leandrojg@users.noreply.github.com>
2021-06-08 22:46:21 -0300

    Ruta Search con querys

--- src/controllers/search/search.ts
+++ src/controllers/search/search.ts
@@ -1,7 +1,16 @@
-import express from 'express'
+import express from "express";
 
-function searchController (req:express.Request, res:express.Response){
-    res.send('search')
+async function searchController(req: express.Request, res: express.Response) {
+  const product = req.query.product;
+  if (Object.keys(req.query).length !== 0 ) { // Checkea si hay query o no.
+      console.log("query", req.query)
+    if (product) { //Checkea si hay product.
+      return res.send(`Buscaste: ${product}`);
+    } else { //Si existe query pero esta mal solicitada, enviá bad request
+      return res.status(400).send(`Bad request: ${req.query[0]}`);
+    }
+  }
+  return res.send("main route")
 }
 
-export default searchController
+export default searchController;
