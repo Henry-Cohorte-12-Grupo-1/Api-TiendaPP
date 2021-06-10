50640a4e229d2369a2207e2793a42ed8d9e7c1a7
manu naga <nagami.mn@gmail.com>
2021-06-10 12:57:53 -0300

    image seeder added, product seeders: productId changed to UUIDv4

---
+++ src/seeders/image.ts
@@ -0,0 +1,26 @@
+export const images = [
+    {
+        imageId: "K2543VH",
+        productId: "13c22a6b-fa8e-4640-ad5c-132e0e61a4f3",
+    },
+    {
+        imageId: "J5H6VG2V13",
+        productId: "13c22a6b-fa8e-4640-ad5c-132e0e61a4f3",
+    },
+    {
+        imageId: "JHCYC5612",
+        productId: "13c22a6b-fa8e-4640-ad5c-132e0e61a4f3",
+    },
+    {
+        imageId: "JHCYC5612",
+        productId: "cce405a8-873b-4ceb-a9b1-8fd6f4baf250",
+    },
+    {
+        imageId: "JHCYC5612",
+        productId: "cce405a8-873b-4ceb-a9b1-8fd6f4baf250",
+    },
+    {
+        imageId: "JHCYC5612",
+        productId: "9a09e1fa-e7ae-47bd-acb3-0bffaf012d81",
+    },
+];
--- src/seeders/product.ts
+++ src/seeders/product.ts
@@ -1,73 +1,68 @@
 export const products = [
     {
-        productId: 1,
-        name:'TCL 50-inch Class 4-Series 4K UHD Smart Roku LED TV - 50S435, 2021 Model',
+        productId: "13c22a6b-fa8e-4640-ad5c-132e0e61a4f3",
+        name: "TCL 50-inch Class 4-Series 4K UHD Smart Roku LED TV - 50S435, 2021 Model",
         price: 348,
-        status: 'in_stock',
+        status: "in_stock",
         quantity: 80,
-        description:"Television 50'"
-
+        description: "Television 50'",
     },
 
     {
-        productId: 2,
-        name:'2021 Apple TV 4K (32GB)',
+        productId: "cce405a8-873b-4ceb-a9b1-8fd6f4baf250",
+        name: "2021 Apple TV 4K (32GB)",
         price: 179,
-        status: 'running_low',
+        status: "running_low",
         quantity: 9,
-        description:'Apple TV'
-
+        description: "Apple TV",
     },
 
     {
-        productId: 3,
-        name:'Oculus - Rift S PC-Powered VR Gaming Headset',
+        productId: "9a09e1fa-e7ae-47bd-acb3-0bffaf012d81",
+        name: "Oculus - Rift S PC-Powered VR Gaming Headset",
         price: 179,
         quantity: 0,
-        description:'Video-Reality Headset'
-
+        description: "Video-Reality Headset",
     },
     {
-        productId: 4,
-        name:'Tea Cup Mug Lid Infuser Stainless Steel Filter 500ml',
-        price: 38.50,
+        productId: "2840efc4-c172-4961-bf8d-78c1bad3d1ee",
+        name: "Tea Cup Mug Lid Infuser Stainless Steel Filter 500ml",
+        price: 38.5,
         quantity: 139,
-        description:'Mug'
-
+        description: "Mug",
     },
     {
-        productId: 5,
-        name:'Burgess Insect Mosquito Bug Propane Fogger Sprayer',
+        productId: "41e63e10-b85b-4a2f-a41c-27434fa35a9f",
+        name: "Burgess Insect Mosquito Bug Propane Fogger Sprayer",
         price: 85.45,
-        quantity: 4
+        quantity: 4,
     },
     {
-        productId: 6,
-        name:'Electronic Meat Thermometer Kitchen Tools Digital Food Thermometer',
+        productId: "c04d0fa6-a561-470b-9907-529ee916ca0d",
+        name: "Electronic Meat Thermometer Kitchen Tools Digital Food Thermometer",
         price: 3,
         quantity: 0,
-        description:'Digital Kitchen Meat Thermometer'
+        description: "Digital Kitchen Meat Thermometer",
     },
     {
-        productId: 7,
-        name:"Victoria's Secret Pink Backpack",
+        productId: "32cf621a-9551-43ac-81fe-150938dd706f",
+        name: "Victoria's Secret Pink Backpack",
         price: 59.97,
         quantity: 92,
-        description:'Backpack'
+        description: "Backpack",
     },
     {
-        productId: 8,
-        name:"Original Mercedes-Benz Eau De Toilette Cologne Men",
+        productId: "3490bb8b-3a0d-41a2-86aa-32922c57edbb",
+        name: "Original Mercedes-Benz Eau De Toilette Cologne Men",
         price: 28.75,
         quantity: 7,
-        description:'Perfume'
+        description: "Perfume",
     },
     {
-        productId: 9,
-        name:"Conair Custom Cut 18pc Home Haircutting Kit",
+        productId: "3d56de88-5f25-433f-9c3c-1dfa30cce15a",
+        name: "Conair Custom Cut 18pc Home Haircutting Kit",
         price: 44,
         quantity: 0,
-        description:'Haircutting kit'
-    }
-    
+        description: "Haircutting kit",
+    },
 ];
