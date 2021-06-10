383fdc9e050a7733085ef341fb288aedb17d7ad0
Jupaolivera <75101920+Jupaolivera@users.noreply.github.com>
2021-06-10 13:22:53 -0300

    added Category controller

---
+++ src/controllers/category/category.ts
@@ -0,0 +1,12 @@
+import app from "../../app";
+import db from "../../models";
+import { categories } from "../../seeders/category";
+
+
+//For testing purposes, it loads users from the seeders to DB
+export const createDummyCategories =  () => {
+    categories.map(async (category) => {
+        await db.Category.create(category);
+    });
+};
+
