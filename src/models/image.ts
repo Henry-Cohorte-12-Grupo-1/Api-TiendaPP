27d295dbd0489879be15c824fb3aebb48883c7fe
Jupaolivera <75101920+Jupaolivera@users.noreply.github.com>
2021-06-10 00:10:25 -0300

    added Image model with associations to Product model

---
+++ src/models/image.ts
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+
+
+interface ImageAttributes{
+  imageId: string
+}
+
+module.exports = (sequelize: any, DataTypes: any) => {
+  class Image extends Model<ImageAttributes> implements ImageAttributes {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    imageId!: string;
+
+    static associate(models: any) {
+      // define association here
+      Image.belongsTo(models.Product)
+    }
+  };
+  Image.init({
+    imageId: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Image',
+  });
+  return Image;
+};
