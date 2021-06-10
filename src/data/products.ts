aeb629d471db3671c194317bf8ce31276f7920e2
unknown <nahuecroc@gmail.com>
2021-06-10 10:56:52 -0300

    Categorias agregadas a productos

--- src/data/products.ts
+++ src/data/products.ts
@@ -4,60 +4,69 @@ module.exports = [
         image: 'https://http2.mlstatic.com/D_NQ_NP_934496-MLA45625835121_042021-O.webp',
         description: 'Con el Smart TV vas a acceder a las aplicaciones en las que se encuentran tus contenidos favoritos.',
         price: 31999,
+        category: 'Electronics'
     },
     {
         name: 'Notebook Hp 255 G7',
         image: 'https://http2.mlstatic.com/D_NQ_NP_786229-MLA46010757671_052021-O.webp',
         description: 'Notebook Hp 255 G7 Amd Athlon 3150u 8gb 1tb 15,6 pulg. Graficos Radeon',
         price: 66499,
+        category: 'Electronics'
     },
     {
         name: 'Taladro eléctrico percutor y destornillador Lüsqtoff',
         image: 'https://http2.mlstatic.com/D_NQ_NP_923962-MLA44666427411_012021-O.webp',
         description: 'Con el taladro eléctrico Lüsqtoff ATL18-8B podrás realizar múltiples tareas en diversas superficies de un modo práctico y sencillo.',
         price: 10700,
+        category: 'Electronics'
     },
     {
         name: 'Microondas Atma Vintage',
         image: 'https://http2.mlstatic.com/D_NQ_NP_640576-MLA41816931234_052020-O.webp',
         description: '¿Necesitás soluciones rápidas? Disfrutá de tus comidas favoritas en tan solo minutos con la línea Atma.',
         price: 13999,
+        category: 'Electronics'
     },
     {
         name: 'Bicicleta Nordic X3',
         image: 'https://http2.mlstatic.com/D_NQ_NP_850765-MLA45120463588_032021-O.webp',
         description: 'Bicicleta Nordic X 3.0 By SLP 21v R29 Disco Mecanico',
-        price: 37900
+        price: 37900,
+        category: 'Sport'
     },
     {
         name: "Brown eggs",
         image: "https://http2.mlstatic.com/D_NQ_NP_890407-MLA44426449288_122020-O.webp",
         description: "Raw organic brown eggs in a basket",
         price: 28.1,
+        category: 'Food'
     },
     {
         name: "Sweet fresh stawberry",
         image: "https://http2.mlstatic.com/D_NQ_NP_769458-MLA43542840180_092020-O.webp",
         description: "Sweet fresh stawberry on the wooden table",
         price: 29.45,
-        
+        category: 'Food'
     },
     {
         name: "Asparagus",
         image: "https://http2.mlstatic.com/D_NQ_NP_889155-MLA40425215712_012020-O.webp",
         description: "Asparagus with ham on the wooden table",
         price: 18.95,
+        category: 'Food'
     },
     {
         name: "Green smoothie",
         image: "https://http2.mlstatic.com/D_NQ_NP_968302-MLA43483582148_092020-O.webp",
         description: "Glass of green smoothie with quail egg's yolk, served with cocktail tube, green apple and baby spinach leaves over tin surface.",
         price: 17.68,
+        category: 'Food'
     },
     {
         name: "Raw legums",
         image:"https://http2.mlstatic.com/D_NQ_NP_718170-MLA42228741383_062020-O.webp",
         description: "Raw legums on the wooden table",
         price: 17.11,
+        category: 'Food'
       }
 ]
