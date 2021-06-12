d584ef450d4be4e1cc0e97a3f74ef84d3ae68b48
Pedro <pedro.maresc@gmail.com>
2021-06-12 03:55:04 -0300

    changed seeders to only have cloudinary's imageId. Updated model Image

--- src/seeders/image.ts
+++ src/seeders/image.ts
@@ -1,32 +1,26 @@
 export const images = [
     {
-        imageId: "K2543VH",
+        imageId: "lvstjh4mf6ajr72dvseh",
         productId: "13c22a6b-fa8e-4640-ad5c-132e0e61a4f3",
-        url: "https://http2.mlstatic.com/D_NQ_NP_934496-MLA45625835121_042021-O.webp"
     },
     {
-        imageId: "J5H6VG2V13",
+        imageId: "tpkvs4nrf4tyjawpf2qv",
         productId: "13c22a6b-fa8e-4640-ad5c-132e0e61a4f3",
-        url: "https://http2.mlstatic.com/D_NQ_NP_786229-MLA46010757671_052021-O.webp"
     },
     {
-        imageId: "JHCYC5612",
+        imageId: "ixmctfqll30bfquyisbt",
         productId: "13c22a6b-fa8e-4640-ad5c-132e0e61a4f3",
-        url: "https://http2.mlstatic.com/D_NQ_NP_923962-MLA44666427411_012021-O.webp"
     },
     {
-        imageId: "JHCYC5612",
+        imageId: "kd2bvuvcdsh4igbe9xtc",
         productId: "cce405a8-873b-4ceb-a9b1-8fd6f4baf250",
-        url: "https://http2.mlstatic.com/D_NQ_NP_640576-MLA41816931234_052020-O.webp"
     },
     {
-        imageId: "JHCYC5612",
+        imageId: "b7tcn1mrbh9sg8sfmy21",
         productId: "cce405a8-873b-4ceb-a9b1-8fd6f4baf250",
-        url: "https://http2.mlstatic.com/D_NQ_NP_850765-MLA45120463588_032021-O.webp"
     },
     {
-        imageId: "JHCYC5612",
+        imageId: "uog9apc3tiszomoglniq",
         productId: "9a09e1fa-e7ae-47bd-acb3-0bffaf012d81",
-        url: "https://http2.mlstatic.com/D_NQ_NP_769458-MLA43542840180_092020-O.webp"
     },
 ];
