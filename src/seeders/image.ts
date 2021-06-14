3bc9e7bc49d81e7d3967451eeba4ff8a42b6425a
Pedro <pedro.maresc@gmail.com>
2021-06-14 09:30:36 -0300

    Updated seeders

--- src/seeders/image.ts
+++ src/seeders/image.ts
@@ -23,4 +23,49 @@ export const images = [
         imageId: "uog9apc3tiszomoglniq",
         productId: "9a09e1fa-e7ae-47bd-acb3-0bffaf012d81",
     },
+    {
+        imageId: "y6wevpckgqvf7gha1v5c",
+        productId: "3490bb8b-3a0d-41a2-86aa-32922c57edbb",
+    },
+    {
+        imageId: "usdtmr2gqksdnvhevvhr",
+        productId: "41e63e10-b85b-4a2f-a41c-27434fa35a9f",
+    },
+    {
+        imageId: "b0azhpctcdhclcmaejqc",
+        productId: "41e63e10-b85b-4a2f-a41c-27434fa35a9f",
+    },
+    {
+        imageId: "la7uawebkxlzj3tenisy",
+        productId: "41e63e10-b85b-4a2f-a41c-27434fa35a9f",
+    },
+    {
+        imageId: "fcvqq3mlo0vwoymt6jnn",
+        productId: "3d56de88-5f25-433f-9c3c-1dfa30cce15a",
+    },
+
+
+
+    {
+        imageId: "t0eqrvs1zjci5e4t36jb",
+        productId: "9a09e1fa-e7ae-47bd-acb3-0bffaf012d81",
+    },
+
+    {
+        imageId: "c5gnqj7jcdtzldm9hahq",
+        productId: "c04d0fa6-a561-470b-9907-529ee916ca0d",
+    },
+
+    {
+        imageId: "vleebuhyirot5h9cm0qg",
+        productId: "32cf621a-9551-43ac-81fe-150938dd706f",
+    },
+    {
+        imageId: "ymmi8mxhnh8klava0voe",
+        productId: "2840efc4-c172-4961-bf8d-78c1bad3d1ee",
+    },
+
+
+
+
 ];
--- src/seeders/review.ts
+++ src/seeders/review.ts
@@ -7,7 +7,12 @@ export const reviews = [
     {
         productId: "13c22a6b-fa8e-4640-ad5c-132e0e61a4f3",
         score: 3,
-        review: "lklfsajlkfsajkfsajklñasdfjlksa"
+        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        productId: "cce405a8-873b-4ceb-a9b1-8fd6f4baf250",
+        score: 5,
+        review: "Esto no sé ni qué producto es. Sólo sé que cada que vez que entro a esta página me muero de ganas de comprarlo, qué buena que está esta página, quiero dejarles todo mi dinero."
     },
     {
         productId: "cce405a8-873b-4ceb-a9b1-8fd6f4baf250",
@@ -17,6 +22,79 @@ export const reviews = [
     {
         productId: "9a09e1fa-e7ae-47bd-acb3-0bffaf012d81",
         score: 5,
-        review: "Buen producto, rey"
+        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
+    {
+        productId: "cce405a8-873b-4ceb-a9b1-8fd6f4baf250",
+        score: 5,
+        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        productId: "9a09e1fa-e7ae-47bd-acb3-0bffaf012d81",
+        score: 5,
+        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        productId: "13c22a6b-fa8e-4640-ad5c-132e0e61a4f3",
+        score: 4,
+        review: "Buen producto, rey. Me encanta todo lo que publicas bb. Volvería a comprarlo mil veces mas"
+    },
+    {
+        productId: "13c22a6b-fa8e-4640-ad5c-132e0e61a4f3",
+        score: 3,
+        review: "La compra todo bien pero faltaría algo de calidad en los detalles, pero lo recomiendo."
+    },
+
+    {
+        productId: "cce405a8-873b-4ceb-a9b1-8fd6f4baf250",
+        score: 3,
+        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+
+    {
+        productId: "9a09e1fa-e7ae-47bd-acb3-0bffaf012d81",
+        score: 3,
+        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+
+    {
+        productId: "2840efc4-c172-4961-bf8d-78c1bad3d1ee",
+        score: 3,
+        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+
+    {
+        productId: "41e63e10-b85b-4a2f-a41c-27434fa35a9f",
+        score: 3,
+        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+
+    {
+        productId: "c04d0fa6-a561-470b-9907-529ee916ca0d",
+        score: 3,
+        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+
+    {
+        productId: "32cf621a-9551-43ac-81fe-150938dd706f",
+        score: 3,
+        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+
+    {
+        productId: "3490bb8b-3a0d-41a2-86aa-32922c57edbb",
+        score: 3,
+        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+
+    {
+        productId: "3d56de88-5f25-433f-9c3c-1dfa30cce15a",
+        score: 3,
+        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+
+
+
+
+
 ];
