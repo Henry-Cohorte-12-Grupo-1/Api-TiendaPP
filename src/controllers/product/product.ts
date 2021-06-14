9500e7f4445ae47fa7c2f141e3dce10f908df356
Pedro <pedro.maresc@gmail.com>
2021-06-14 02:48:52 -0300

    Now when creating a new product it creates associated images

--- src/controllers/product/product.ts
+++ src/controllers/product/product.ts
@@ -24,39 +24,46 @@ export const createDummyProducts = () => {
 };
 
 
-export async function createProduct (req:Request, res:Response){
-    
-    //categorias de prueba 
-    let name = req.body.name
-    let description = req.body.description
-    let price = parseInt(req.body.price)
-    let images = req.body.images
-    let categoryId = parseInt(req.body.categoryId)
-    let quantity = parseInt(req.body.quantity)
+export async function createProduct(req: Request, res: Response) {
+
+    let { name, description, price, categoryId, images, quantity } = req.body
+    //categorias de prueba
+    const imagesArr: string[] = images && images.split(" - ")
+
+    price = parseInt(req.body.price)
+    categoryId = parseInt(req.body.categoryId)
+    quantity = parseInt(req.body.quantity)
+
     // console.log('IN', name, description,price, images,categoryId,quantity)
-    let create =   await db.Product.create({
-            name: req.body.name,
-            price: price,
-            description: req.body.description,
-            userId: "6d2ba377-b219-4925-b6df-4cbc8575ce50",
-            quantity: quantity,
-            categoryId: categoryId,
+    const productId = uuidv4()
+    const productCreated = await db.Product.create({
+        productId: productId,
+        name: name,
+        price: price,
+        description: description,
+        userId: "68f58789-37b2-4a60-838e-93c8eedf7fcc",
+        quantity: quantity,
+        categoryId: categoryId,
 
-        }).catch((error:any) => (console.log(error)))
-    console.log(create)
-    res.send(create)
-    // console.log({name,description,price,images,categoryId})
+    }).catch((error: any) => (console.log(error)))
+    console.log(productCreated)
 
-    db
+    imagesArr && imagesArr.forEach(async id => {
+        await db.Image.create({
+            imageId: id,
+            productId: productId,
+        })
+    })
+    res.send(productId)
 
 }
 
 
-export async function getProduct(req:Request , res:Response) {
-    let {product} = req.query
+export async function getProduct(req: Request, res: Response) {
+    let { product } = req.query
     // let resp = await db.Product.findAll({include:db.Image})
 
-    let resp = await db.Product.findByPk(product,{include:db.Image})
+    let resp = await db.Product.findByPk(product, { include: db.Image })
 
     res.send(resp)
 }
