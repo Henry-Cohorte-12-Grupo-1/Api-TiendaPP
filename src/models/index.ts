15afa4b88c15f0b58769b1354cde4fe6a543032b
manu naga <nagami.mn@gmail.com>
2021-06-16 10:52:19 -0300

    fixed model index for debugging (now parsing .js)

--- src/models/index.ts
+++ src/models/index.ts
@@ -14,10 +14,9 @@ if (config.use_env_variable) {
 } else {
     sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
-
 fs.readdirSync(__dirname)
     .filter((file: string) => {
-        return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.ts';
+        return file.indexOf('.') !== 0 && file !== basename && (file.slice(-3) === '.ts' || file.slice(-3) === '.js');
     })
     .forEach((file: any) => {
         const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
