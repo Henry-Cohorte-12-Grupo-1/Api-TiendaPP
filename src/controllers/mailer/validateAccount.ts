6a87b40d3c64deceef613108e8276cb6a8968d23
Tomás Garcia <tomy.garcia@hotmail.com>
2021-06-17 00:07:06 -0300

    validation fixed

--- src/controllers/mailer/validateAccount.ts
+++ src/controllers/mailer/validateAccount.ts
@@ -5,15 +5,19 @@ import {Role} from '../../interfaces/role'
 const validateAccount = async (req: express.Request, res: express.Response) => {
     const {id} = req.query;
     console.log(id)
-    
-    let resp = await db.Users.update({ role: Role.user}, {
-        where: {
-          code: id
-        }
-      });
-      
-      console.log(resp)
-    res.send({resp})
+
+    let resp = await db.User.update({ role: Role.user }, {
+      where: {
+        code: id
+      }
+    })
+    if(resp[0]==1){
+      res.send('verificado')
+    } else {
+      res.send('error')
+    }
+
+
 }
 
 export default validateAccount;
