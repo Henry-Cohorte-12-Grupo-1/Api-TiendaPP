0892641246368683ecf6ed44e4f4d02b4054eea3
Pedro <pedro.maresc@gmail.com>
2021-06-18 06:14:57 -0300

    created user's sales orders route with controllers

--- src/routes/orders.ts
+++ src/routes/orders.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
 import ordersController from '../controllers/orders/orders'
+import salesController from '../controllers/orders/sales'
 
 
 const orders = Router()
 
+
+orders.use('/sales/:userName', salesController)
 orders.use('/:userName', ordersController)
 
 export default orders;
