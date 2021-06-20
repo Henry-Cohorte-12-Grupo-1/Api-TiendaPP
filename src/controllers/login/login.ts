0fa3e9ecdc0039c03e702f95cc946ac4e2abe97d
Tomás Garcia <tomy.garcia@hotmail.com>
2021-06-19 21:08:23 -0300

    force password response on login route

--- src/controllers/login/login.ts
+++ src/controllers/login/login.ts
@@ -1,77 +1,81 @@
-import express, { Request, Response} from 'express'; 
+import express, { Request, Response } from 'express';
 import db from '../../models';
 
 import { IEmail } from '../../interfaces/mailer';
-import {Mailer} from '../mailer/nodeMailer'
+import { Mailer } from '../mailer/nodeMailer'
 import jwt from 'jsonwebtoken';
 import config from '../../lib/config';
 
 
 
-function createToken(user:any) {
-    return jwt.sign({ id: user.userId,username: user.username, email: user.email, user:true}, config.JWT_SECRET, {
-      expiresIn: 86400
+function createToken(user: any) {
+    return jwt.sign({ id: user.userId, username: user.username, email: user.email, user: true }, config.JWT_SECRET, {
+        expiresIn: 86400
     });
 }
 
-function createTokenVerification(user: any, key:number) {
-    return jwt.sign({ id: user.userId,username: user.username, email: user.email, key:key}, 'key', {
+function createTokenVerification(user: any, key: number) {
+    return jwt.sign({ id: user.userId, username: user.username, email: user.email, key: key }, 'key', {
         expiresIn: 86400
-      });
+    });
 }
 
 
 const login = async (req: express.Request, res: express.Response) => {
     const key = Math.floor(100000 + Math.random() * 900000);
-    
-    const { email, pass }= req.body;
+
+    const { email, pass } = req.body;
 
     console.log(`email: ${email} pass: ${pass}`)
 
-    const resp = await db.User.findOne({ where: {email: email}})
+    const resp = await db.User.findOne({ where: { email: email } })
     console.log('--------------------------------')
-    console.log(resp);
+    console.log(resp?.forcePassword);
     console.log('--------------------------------')
 
 
 
 
-    let mailFormat:IEmail = {      
-            from: '"TiendApp" <tomygaar@gmail.com>', // sender address
-            to: `${email}, tomasqgarcia@gmail.com`, // list of receivers
-            subject: "Two-Steps Validation", // Subject line
-            text: "Two-Steps Validation", // plain text body
-            html: `<b>Welcome ${email}!</b>
+    let mailFormat: IEmail = {
+        from: '"TiendApp" <tomygaar@gmail.com>', // sender address
+        to: `${email}, tomasqgarcia@gmail.com`, // list of receivers
+        subject: "Two-Steps Validation", // Subject line
+        text: "Two-Steps Validation", // plain text body
+        html: `<b>Welcome ${email}!</b>
             <p>Please use the following code to complete your two steps validation on TiendApp page:</p>
             <p>Your 6 digits key: ${key}<p>`, // html body
     }
 
-    if(resp && resp.password === pass){
-        let answer = {}
-        switch(resp.role){
-            case 1:
-                Mailer(mailFormat)
-                answer = {
-                    message: 'Admin',
-                    key: key,
-                    token: createTokenVerification(resp,key)
-                }
-                break;
-            case 2:
-                answer = {
-                    message: 'User',
-                    token: createToken(resp)
-                }
-                break;
-            case 3:
-                answer = {
-                    message: 'Disabled'
-                }
-                break;
+    if (resp.forcePassword) {
+        res.send({message:'password reset'})
+    } else {
+        if (resp && resp.password === pass) {
+            let answer = {}
+            switch (resp.role) {
+                case 1:
+                    Mailer(mailFormat)
+                    answer = {
+                        message: 'Admin',
+                        key: key,
+                        token: createTokenVerification(resp, key)
+                    }
+                    break;
+                case 2:
+                    answer = {
+                        message: 'User',
+                        token: createToken(resp)
+                    }
+                    break;
+                case 3:
+                    answer = {
+                        message: 'Disabled'
+                    }
+                    break;
+            }
+            res.send(answer)
         }
-        res.send(answer)
+        res.send({ message: 'User or password are incorrect' });
     }
-    res.send({message: 'User or password are incorrect'});
 }
 
 export default login;
