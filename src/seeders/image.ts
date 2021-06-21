197f7fa76cff230c09c7916e91c3d64365a4f55f
leandrojg <79561670+leandrojg@users.noreply.github.com>
2021-06-21 07:04:31 -0300

    los que me olvide de guardar

--- src/seeders/image.ts
+++ src/seeders/image.ts
@@ -1,68 +1,64 @@
 export const images = [
-    {
-        imageId: "dvyddtnftabavzbev4yx",
-        productId: "2840efc4-c172-4961-bf8d-78c1bad3d1ee",
-    },
-    {
-        imageId: "ph4gxrss5oegofbvhp8e",
-        productId: "13c22a6b-fa8e-4640-ad5c-132e0e61a4f3",
-    },
-    {
-        imageId: "kwd9cdmnpip1otqkoiqk",
-        productId: "13c22a6b-fa8e-4640-ad5c-132e0e61a4f3",
-    },
-    {
-        imageId: "rxvmgqsp4aygelsancg1",
-        productId: "cce405a8-873b-4ceb-a9b1-8fd6f4baf250",
-    },
-    {
-        imageId: "mohmuoq3g2plw7h3fnfi",
-        productId: "cce405a8-873b-4ceb-a9b1-8fd6f4baf250",
-    },
-    {
-        imageId: "zqwl66kukowyhj5ku9oq",
-        productId: "9a09e1fa-e7ae-47bd-acb3-0bffaf012d81",
-    },
-    {
-        imageId: "y6wevpckgqvf7gha1v5c",
-        productId: "3490bb8b-3a0d-41a2-86aa-32922c57edbb",
-    },
-    {
-        imageId: "usdtmr2gqksdnvhevvhr",
-        productId: "41e63e10-b85b-4a2f-a41c-27434fa35a9f",
-    },
-    {
-        imageId: "b0azhpctcdhclcmaejqc",
-        productId: "41e63e10-b85b-4a2f-a41c-27434fa35a9f",
-    },
-    {
-        imageId: "la7uawebkxlzj3tenisy",
-        productId: "41e63e10-b85b-4a2f-a41c-27434fa35a9f",
-    },
-    {
-        imageId: "fcvqq3mlo0vwoymt6jnn",
-        productId: "3d56de88-5f25-433f-9c3c-1dfa30cce15a",
-    },
-    {
-        imageId: "c0guip4dmvubt6qo0rrf",
-        productId: "9a09e1fa-e7ae-47bd-acb3-0bffaf012d81",
-    },
+  {
+    imageId: "dvyddtnftabavzbev4yx",
+    productId: "2840efc4-c172-4961-bf8d-78c1bad3d1ee",
+  },
+  {
+    imageId: "ph4gxrss5oegofbvhp8e",
+    productId: "13c22a6b-fa8e-4640-ad5c-132e0e61a4f3",
+  },
+  {
+    imageId: "kwd9cdmnpip1otqkoiqk",
+    productId: "13c22a6b-fa8e-4640-ad5c-132e0e61a4f3",
+  },
+  {
+    imageId: "rxvmgqsp4aygelsancg1",
+    productId: "cce405a8-873b-4ceb-a9b1-8fd6f4baf250",
+  },
+  {
+    imageId: "mohmuoq3g2plw7h3fnfi",
+    productId: "cce405a8-873b-4ceb-a9b1-8fd6f4baf250",
+  },
+  {
+    imageId: "zqwl66kukowyhj5ku9oq",
+    productId: "9a09e1fa-e7ae-47bd-acb3-0bffaf012d81",
+  },
+  {
+    imageId: "y6wevpckgqvf7gha1v5c",
+    productId: "3490bb8b-3a0d-41a2-86aa-32922c57edbb",
+  },
+  {
+    imageId: "usdtmr2gqksdnvhevvhr",
+    productId: "41e63e10-b85b-4a2f-a41c-27434fa35a9f",
+  },
+  {
+    imageId: "b0azhpctcdhclcmaejqc",
+    productId: "41e63e10-b85b-4a2f-a41c-27434fa35a9f",
+  },
+  {
+    imageId: "la7uawebkxlzj3tenisy",
+    productId: "41e63e10-b85b-4a2f-a41c-27434fa35a9f",
+  },
+  {
+    imageId: "fcvqq3mlo0vwoymt6jnn",
+    productId: "3d56de88-5f25-433f-9c3c-1dfa30cce15a",
+  },
+  {
+    imageId: "c0guip4dmvubt6qo0rrf",
+    productId: "9a09e1fa-e7ae-47bd-acb3-0bffaf012d81",
+  },
 
-    {
-        imageId: "c5gnqj7jcdtzldm9hahq",
-        productId: "c04d0fa6-a561-470b-9907-529ee916ca0d",
-    },
+  {
+    imageId: "c5gnqj7jcdtzldm9hahq",
+    productId: "c04d0fa6-a561-470b-9907-529ee916ca0d",
+  },
 
-    {
-        imageId: "vleebuhyirot5h9cm0qg",
-        productId: "32cf621a-9551-43ac-81fe-150938dd706f",
-    },
-    {
-        imageId: "ymmi8mxhnh8klava0voe",
-        productId: "2840efc4-c172-4961-bf8d-78c1bad3d1ee",
-    },
-
-
-
-
+  {
+    imageId: "vleebuhyirot5h9cm0qg",
+    productId: "32cf621a-9551-43ac-81fe-150938dd706f",
+  },
+  {
+    imageId: "ymmi8mxhnh8klava0voe",
+    productId: "2840efc4-c172-4961-bf8d-78c1bad3d1ee",
+  },
 ];
