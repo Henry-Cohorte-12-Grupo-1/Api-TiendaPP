25816f14020151425b765f8c73ba3bf92442edd9
Pedro <pedro.maresc@gmail.com>
2021-06-24 00:51:50 -0300

    added stock management on payment

--- src/controllers/payment/post-pay.ts
+++ src/controllers/payment/post-pay.ts
@@ -1,14 +1,16 @@
 import express from "express";
 import deleteCart from "./helpers/delete"
 import order from "./helpers/order"
+import stock from "./helpers/stock";
 //import stock from "./helpers/stock"
 //import mailer from "./helpers/mailer"
-//import { payController } from "./stripe";
+//import { payController } fromn "./stripe";
 
 async function postPayController(req: express.Request, res: express.Response) {
-    const { userId, items } = req.body
+    const { userId, quantity, items, productId } = req.body
     deleteCart(userId)
     order(items, userId)
+    stock(items)
     //console.log(req.body)
     return res.send("postpay :D")
 }
