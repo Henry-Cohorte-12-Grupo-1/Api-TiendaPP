d274b249d01fd38aa6b6b0aedad21ffe9e414742
Pedro <pedro.maresc@gmail.com>
2021-06-26 04:04:15 -0300

    now if a profile exists it updates it

--- src/controllers/sellerProfile/newSellerProfile.ts
+++ src/controllers/sellerProfile/newSellerProfile.ts
@@ -11,19 +11,45 @@ async function newSellerProfile(
     //
     const { header, description, userId, images } = req.body
     try {
-        const newProfile = await db.SellerProfile.create({
-            header: header,
-            description: description,
-            userId: userId
-        })
 
-        if (images.length) {
-            images.forEach((image: any) => db.SellerProfileImage.create({
-                url: image,
-                sellerProfileId: newProfile.sellerProfileId
-            }))
+        // --> images [1, 2, 3, 4]
+        // --> imges [2, 3, 5]
+
+        const profile = await db.SellerProfile.findOne({ where: { userId: userId } })
+        if (profile) {
+            /*
+            profile.header = header
+            profile.description = description
+            */
+            db.SellerProfile.update({ header: header, description: description }, { where: { userId: userId } })
+
+            const sellerProfileId = profile.sellerProfileId
+            await db.SellerProfileImages.destroy({ where: { userId: userId } })
+            if (images.length) {
+                images.forEach(async (image: string) => await db.SellerProfileImages.create({ sellerProfileId: sellerProfileId, url: image }))
+            }
+            res.send(profile)
+        } else {
+            const newProfile = await db.SellerProfile.create({
+                header: header,
+                description: description,
+                userId: userId
+            })
+
+            if (images.length) {
+                images.forEach((image: any) => db.SellerProfileImage.create({
+                    url: image,
+                    sellerProfileId: newProfile.sellerProfileId
+                }))
+            }
+            res.send(newProfile)
         }
-        res.send(newProfile)
+
+
+
+
+
+
     } catch (error: any) {
         console.log("caught", error.message)
         return res.send(error.message);
