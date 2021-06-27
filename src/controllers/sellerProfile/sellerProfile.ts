a14631b7293d82a9425a4fc5ecbc42b2b2ccb5fe
Pedro <pedro.maresc@gmail.com>
2021-06-27 00:38:50 -0300

    Fixed errors

--- src/controllers/sellerProfile/sellerProfile.ts
+++ src/controllers/sellerProfile/sellerProfile.ts
@@ -18,16 +18,16 @@ async function sellerPageProducts(req: express.Request, res: express.Response) {
     const profile = await db.SellerProfile.findOne({
       where: { userId: sellerId }
     })
-
-    const allImages: any = await db.SellerProfileImage.findAll({
-      where: {
-        sellerProfileId: profile.sellerProfileId
-      }
-    }).then((ims: any[]) => ims.map((i: any) => i.url));
-
-    const result: any = { ...profile.dataValues, images: allImages }
-
-    res.send(result);
+    if (profile) {
+      const allImages: any = await db.SellerProfileImage.findAll({
+        where: {
+          sellerProfileId: profile.sellerProfileId
+        }
+      }).then((ims: any[]) => ims.map((i: any) => i.url));
+      const result: any = { ...profile.dataValues, images: allImages }
+      return res.send(result);
+    }
+    else return res.send({ error: "User has not made a page yet" })
   } catch (error: any) {
     return res.send(error.message);
   }
