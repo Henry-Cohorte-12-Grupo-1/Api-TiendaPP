c1e6bc236ae3bfea1b8f394faedab09eb8a434b3
Tomás Garcia <tomy.garcia@hotmail.com>
2021-06-26 23:17:02 -0300

    update product seeder

--- src/seeders/product.ts
+++ src/seeders/product.ts
@@ -115,7 +115,7 @@ export const products = [
     quantity: 1,
     price: 22.3,
     description: "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
-    categoryId: "men's clothing",
+    categoryId: 1003,
     //: "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
   },
   {
