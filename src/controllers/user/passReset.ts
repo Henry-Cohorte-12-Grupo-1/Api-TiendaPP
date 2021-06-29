13759e45b03365864e6e21d0d934b5edcd3b295c
Jupaolivera <75101920+Jupaolivera@users.noreply.github.com>
2021-06-29 20:40:52 -0300

    added images for new products

--- src/controllers/user/passReset.ts
+++ src/controllers/user/passReset.ts
@@ -2,30 +2,29 @@ import express from "express";
 import db from "../../models";
 
 export const passReset = async (
-
-  req: express.Request,
-  res: express.Response
+    req: express.Request,
+    res: express.Response
 ) => {
-  console.log('entro-----------------------')
-  const { pass, userId } = req.body;
-  //console.log(pass, userId)
-  try {
-    let user = await db.User.update(
-      {
-        password: pass,
-        forcePassword: false,
-      },
-      {
-        where: { userId: userId },
-      }
-    );
-      console.log(user)
-    if (user) {
-      return res.send("succesfully updated");
-    } else {
-      return res.send("error");
+    console.log("entro-----------------------");
+    const { pass, userId } = req.body;
+    //console.log(pass, userId)
+    try {
+        let user = await db.User.update(
+            {
+                password: pass,
+                forcePassword: false,
+            },
+            {
+                where: { userId: userId },
+            }
+        );
+        console.log(user);
+        if (user) {
+            return res.send("succesfully updated");
+        } else {
+            return res.send("error");
+        }
+    } catch (error: any) {
+        console.log("caught", error.message);
     }
-  } catch (error: any) {
-    console.log("caught", error.message);
-  }
 };
