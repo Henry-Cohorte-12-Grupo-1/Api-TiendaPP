935afb8be5048ceb893db683e803d002f10f6bba
Astr0b0i <78230684+Astr0b0i@users.noreply.github.com>
2021-06-30 02:42:59 -0300

    userProducts protected

--- src/routes/userProducts.ts
+++ src/routes/userProducts.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import passport from "passport";
 import userProductsController from "../controllers/user/userProducts";
 
 const userProducts = Router();
 
-userProducts.use("/:userName", userProductsController);
+userProducts.use("/:userName", passport.authenticate("jwt", { session: false }),userProductsController);
 
 export default userProducts;
