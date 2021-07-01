255f57312a634bc8a7d5c9f0be04e663f7d52938
manu naga <nagami.mn@gmail.com>
2021-07-01 19:18:52 -0300

    googleStrat url imported from config url file

--- src/passport.ts
+++ src/passport.ts
@@ -8,136 +8,138 @@ import googleIds from "./config/GOAuthKeys";
 import twitterIds from "./config/GOAuthKeys";
 import facebookIds from "./config/GOAuthKeys";
 import gitHubIds from "./config/GOAuthKeys";
+import { FRONT_URL } from "./config/url";
 
 const BearerStrategy = passportHttpBearer.Strategy;
 const JWTStrategy = passportJWT.Strategy;
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const TwitterStrategy = require('passport-twitter').Strategy;
-const FacebookStrategy = require('passport-facebook').Strategy;
+const TwitterStrategy = require("passport-twitter").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
 const GitHubStrategy = require("passport-github").Strategy;
 
 const opts: passportJWT.StrategyOptions = {
-  jwtFromRequest: passportJWT.ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: config.JWT_SECRET,
+    jwtFromRequest: passportJWT.ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: config.JWT_SECRET,
 };
 
 const optsAdmin: passportJWT.StrategyOptions = {
-  jwtFromRequest: passportJWT.ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: config.JWT_SECRET_ADMIN,
+    jwtFromRequest: passportJWT.ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: config.JWT_SECRET_ADMIN,
 };
 
 const optsVerification: passportJWT.StrategyOptions = {
-  jwtFromRequest: passportJWT.ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: "key",
+    jwtFromRequest: passportJWT.ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: "key",
 };
 
 passport.use(
-  new JWTStrategy(opts, async (payload, done) => {
-    try {
-      const user = await db.User.findOne({
-        where: { username: payload.username },
-      });
-      if (user) {
-        return done(null, user);
-      }
-      return done(null, false);
-    } catch (error) {
-      console.log(error);
-    }
-  })
+    new JWTStrategy(opts, async (payload, done) => {
+        try {
+            const user = await db.User.findOne({
+                where: { username: payload.username },
+            });
+            if (user) {
+                return done(null, user);
+            }
+            return done(null, false);
+        } catch (error) {
+            console.log(error);
+        }
+    })
 );
 
 passport.use(
-  "admin",
-  new JWTStrategy(optsAdmin, async (payload, done) => {
-    try {
-      const user = await db.User.findOne({
-        where: { username: payload.username },
-      });
-      if (user) {
-        return done(null, user);
-      }
-      return done(null, false);
-    } catch (error) {
-      console.log(error);
-    }
-  })
+    "admin",
+    new JWTStrategy(optsAdmin, async (payload, done) => {
+        try {
+            const user = await db.User.findOne({
+                where: { username: payload.username },
+            });
+            if (user) {
+                return done(null, user);
+            }
+            return done(null, false);
+        } catch (error) {
+            console.log(error);
+        }
+    })
 );
 
 passport.use(
-  "verificationKey",
-  new JWTStrategy(optsVerification, async (payload, done) => {
-    try {
-      const user = await db.User.findOne({
-        where: { username: payload.username },
-      });
-      if (user) {
-        return done(null, user);
-      }
-      return done(null, false);
-    } catch (error) {
-      console.log(error);
-    }
-  })
+    "verificationKey",
+    new JWTStrategy(optsVerification, async (payload, done) => {
+        try {
+            const user = await db.User.findOne({
+                where: { username: payload.username },
+            });
+            if (user) {
+                return done(null, user);
+            }
+            return done(null, false);
+        } catch (error) {
+            console.log(error);
+        }
+    })
 );
 
 // Google OAuth
 
 passport.use(
-  new GoogleStrategy(
-    {
-      clientID: googleIds.googleIds.GOOGLE_CLIENT_ID,
-      clientSecret: googleIds.googleIds.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
-    },
-    async function (
-      accessToken: any,
-      refreshToken: any,
-      profile: any,
-      done: any
-    ) {
-      try {
-        console.log('BBBBBBBBBBBBBBBBBBBBBBB Entro al strategy de google')
-        const user = await db.User.findOne({ where: { email: profile.emails[0].value } });
+    new GoogleStrategy(
+        {
+            clientID: googleIds.googleIds.GOOGLE_CLIENT_ID,
+            clientSecret: googleIds.googleIds.GOOGLE_CLIENT_SECRET,
+            callbackURL: `${FRONT_URL}/api/auth/google/callback`,
+        },
+        async function (
+            accessToken: any,
+            refreshToken: any,
+            profile: any,
+            done: any
+        ) {
+            try {
+                const user = await db.User.findOne({
+                    where: { email: profile.emails[0].value },
+                });
 
-        if (user) {
-          return done(null, user.dataValues);
-        } else {
-          const createUser = await db.User.create({
-            username: profile.name.givenName,
-            password: profile.id,
-            email: profile.emails[0].value,
-            firstName: profile.name.givenName,
-            lastName: profile.name.familyName,
-            googleId: profile.id,
-            role: 2,
-          });
-          return done(null, createUser.dataValues);
+                if (user) {
+                    return done(null, user.dataValues);
+                } else {
+                    const createUser = await db.User.create({
+                        username: profile.name.givenName,
+                        password: profile.id,
+                        email: profile.emails[0].value,
+                        firstName: profile.name.givenName,
+                        lastName: profile.name.familyName,
+                        googleId: profile.id,
+                        role: 2,
+                    });
+                    return done(null, createUser.dataValues);
+                }
+            } catch (error: any) {
+                console.log("caught", error.message);
+            }
         }
-      } catch (error: any) {
-        console.log("caught", error.message);
-      }
-    }
-  )
+    )
 );
 
 passport.serializeUser(async (userId: any, done: any) => {
-  try {
-    return done(null, userId);
-  } catch (error: any) {
-    console.log("caught", error.message);
-  }
+    try {
+        return done(null, userId);
+    } catch (error: any) {
+        console.log("caught", error.message);
+    }
 });
 
 passport.deserializeUser(async (id: any, done: any) => {
-  try {
-    const User = await db.User.findByPk(id);
-    if (User) {
-      done(null, User);
+    try {
+        const User = await db.User.findByPk(id);
+        if (User) {
+            done(null, User);
+        }
+    } catch (error: any) {
+        console.log("caught", error.message);
     }
-  } catch (error: any) {
-    console.log("caught", error.message);
-  }
 });
 
 // Twitter Oauth
@@ -176,38 +178,40 @@ passport.deserializeUser(async (id: any, done: any) => {
 // GitHub OAuth
 
 passport.use(
-  new GitHubStrategy(
-    {
-      clientID: gitHubIds.gitHubIds.GITHUB_CLIENT_ID,
-      clientSecret: gitHubIds.gitHubIds.GITHUB_CLIENT_SECRET,
-      callbackURL: "/api/auth/github/callback",
-    },
-    async function (
-      accessToken: any,
-      refreshToken: any,
-      profile: any,
-      done: any
-    ) {
-      const user = await db.User.findOne({ where: { email: profile._json.email } });
+    new GitHubStrategy(
+        {
+            clientID: gitHubIds.gitHubIds.GITHUB_CLIENT_ID,
+            clientSecret: gitHubIds.gitHubIds.GITHUB_CLIENT_SECRET,
+            callbackURL: "/api/auth/github/callback",
+        },
+        async function (
+            accessToken: any,
+            refreshToken: any,
+            profile: any,
+            done: any
+        ) {
+            const user = await db.User.findOne({
+                where: { email: profile._json.email },
+            });
 
-      if (user) {
-        return done(null, user);
-      } else {
-        const createUser = await db.User.create({
-          username: profile._json.login,
-          password: profile.id,
-          email: profile._json.email
-            ? profile._json.email
-            : "noemail@aemail.com",
-          firstName: profile._json.login,
-          lastName: profile._json.login,
-          gitHubId: profile._json.id.toString(),
-          role: 2,
-        });
-        return done(null, createUser);
-      }
-    }
-  )
+            if (user) {
+                return done(null, user);
+            } else {
+                const createUser = await db.User.create({
+                    username: profile._json.login,
+                    password: profile.id,
+                    email: profile._json.email
+                        ? profile._json.email
+                        : "noemail@aemail.com",
+                    firstName: profile._json.login,
+                    lastName: profile._json.login,
+                    gitHubId: profile._json.id.toString(),
+                    role: 2,
+                });
+                return done(null, createUser);
+            }
+        }
+    )
 );
 
 export default passport;
