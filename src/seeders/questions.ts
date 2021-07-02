316b626443d4b03edcb8b133ef9adefb14148b08
Tomás Garcia <tomy.garcia@hotmail.com>
2021-07-01 21:31:43 -0300

    new dummy

--- src/seeders/questions.ts
+++ src/seeders/questions.ts
@@ -3,24 +3,23 @@ export const questions = [
         productId: "13c22a6b-fa8e-4640-ad5c-132e0e61a4f3",
         userId: "9de81b64-1c4a-49d7-b9c5-7554d1d35df2",
         question: 'Hola, se puede conectar a 220v?',
-        answer: 'si, todo piola'
+        answer: 'si, no hay drama'
     },
     {
         productId: "13c22a6b-fa8e-4640-ad5c-132e0e61a4f3",
-        userId: "ef1795e2-c591-4544-8360-ca83cc1943e1",
-        question: 'Hola, se puede conectar a 220v?',
-        answer: 'si, todo piola'
+        userId: "029c1d76-306f-4bf9-bb9c-32cd24533a59",
+        question: 'Viene con antena?',
     },
     {
-        productId: "41e63e10-b85b-4a2f-a41c-27434fa35a9f",
+        productId: "cce405a8-873b-4ceb-a9b1-8fd6f4baf250",
         userId: "9de81b64-1c4a-49d7-b9c5-7554d1d35df2",
-        question: 'Hola, se puede conectar a 220v?',
-        answer: 'si, todo piola'
+        question: 'Es full HD',
+        answer: 'no, es 4k'
     },
     {
-        productId: "41e63e10-b85b-4a2f-a41c-27434fa35a9f",
+        productId: "cce405a8-873b-4ceb-a9b1-8fd6f4baf250",
         userId: "ef1795e2-c591-4544-8360-ca83cc1943e1",
-        question: 'Hola, se puede conectar a 220v?',
-        answer: 'si, todo piola'
+        question: 'Hola, que formato tiene',
+        answer: 'la pantalla es 16:9'
     }
 ]
--- src/seeders/review.ts
+++ src/seeders/review.ts
@@ -1,6 +1,6 @@
 export const reviews = [
   {
-    userId: "6d2ba377-b219-4925-b6df-4cbc8575ce50",
+    userId: "9de81b64-1c4a-49d7-b9c5-7554d1d35df2",
     productId: "13c22a6b-fa8e-4640-ad5c-132e0e61a4f3",
     score: 5,
     review:
@@ -25,22 +25,22 @@ export const reviews = [
     productId: "9a09e1fa-e7ae-47bd-acb3-0bffaf012d81",
     score: 5,
     review:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Muy buenos los lentes parece re real. Lo recomiendo a menos que tengas epilepsia",
   },
   {
-    userId: "6d2ba377-b219-4925-b6df-4cbc8575ce50",
+    userId: "68f58789-37b2-4a60-838e-93c8eedf7fcc",
     productId: "2840efc4-c172-4961-bf8d-78c1bad3d1ee",
-    score: 3,
+    score: 2,
     review:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Se enfria muy rapido, no recomiendo ",
   },
 
   {
-    userId: "6d2ba377-b219-4925-b6df-4cbc8575ce50",
+    userId: "68f58789-37b2-4a60-838e-93c8eedf7fcc",
     productId: "41e63e10-b85b-4a2f-a41c-27434fa35a9f",
-    score: 3,
+    score: 1,
     review:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "No funciona y el olor es horrible",
   },
 
   {
@@ -48,23 +48,23 @@ export const reviews = [
     productId: "c04d0fa6-a561-470b-9907-529ee916ca0d",
     score: 3,
     review:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Para el precio que tiene esta bastante bien",
   },
 
   {
-    userId: "6d2ba377-b219-4925-b6df-4cbc8575ce50",
+    userId: "9de81b64-1c4a-49d7-b9c5-7554d1d35df2",
     productId: "32cf621a-9551-43ac-81fe-150938dd706f",
     score: 3,
     review:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Excelentes materiales, me fui a acampar con ella y ni un problema",
   },
 
   {
-    userId: "6d2ba377-b219-4925-b6df-4cbc8575ce50",
+    userId: "68f58789-37b2-4a60-838e-93c8eedf7fcc",
     productId: "3490bb8b-3a0d-41a2-86aa-32922c57edbb",
     score: 3,
     review:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Lindo perfume ",
   },
   // ,
 
